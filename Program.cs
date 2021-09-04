af8dd15f1831c44827f8dfd4c47d45ba11b8e7eb
sandip ahire <sandipahire34@gmail.com>
2021-09-04 14:27:08 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Params
+{
+    class Program
+    {
+        class param
+        {
+            public int even(params int[] lst)
+            {
+                int count = 0;
+
+                foreach (int x in lst)
+                {
+                    
+                    if (x%2==0)
+                    {
+                        count++;
+                        Console.WriteLine($"{count}:{x}");
+                    }
+                 
+                }
+                return 0;
+                
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            param obj = new param();
+            Console.WriteLine(obj.even(20,34, 7,32,15,9));
+            
+        }
+    }
+}
